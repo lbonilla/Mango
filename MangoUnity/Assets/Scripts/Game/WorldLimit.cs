82c0541b39b5227f278573f846ecc11e5481b68e
Esteban Padilla <ep@estebanpadilla.com>
2013-09-01 09:37:14 -0600

    Add top and botton world limits.

---
+++ MangoUnity/Assets/Scripts/Game/WorldLimit.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// File: WorldLimit
+// Project:
+// Description:
+// Author:Esteban Padilla
+// Email: 
+// Copyright (C) 2013,  (R). All rights reserved.  
+
+public class WorldLimit : MonoBehaviour {
+#region Enums
+#endregion
+
+#region Delegates
+#endregion
+
+#region Constants
+#endregion
+
+#region Public Variables
+#endregion
+
+#region Private Variables
+
+#endregion
+
+#region Properties
+#endregion
+
+#region Constructors
+#endregion
+
+#region Unity Methods
+	void OnTriggerEnter(Collider other) {		 
+		Destroy(other.gameObject);
+    }
+#endregion
+
+#region Game Methods
+#endregion
+
+}
